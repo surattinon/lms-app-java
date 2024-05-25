d1c722045380536e0b335a5e15cdde1af6aaf483
Pumin Wangprasit <savee2011@hotmail.com>
2024-05-26 05:10:06 +0700

    Rename test file.java and Add comment to admin.java

--- src/LMS/admin.java
+++ src/LMS/admin.java
@@ -14,23 +14,29 @@ public class admin {
     }
 
     public static void menuUI() {
+
+        // -- Create panel object and set layout --
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(5, 1, 10, 10));
 
+        // -- Create buttons objects --
         JButton addButton = new JButton("Add Librarian");
         JButton viewButton = new JButton("View Librarian");
         JButton deleteButton = new JButton("Delete Librarian");
         JButton logoutButton = new JButton("Logout");
 
+        // -- Set text lebel font size --
         JLabel titleLebel = new JLabel("Admin Menu", SwingConstants.CENTER);
         titleLebel.setFont(new Font("Arial", Font.BOLD, 24));
 
+        // -- Add buttons to panel --
         panel.add(titleLebel);
         panel.add(addButton);
         panel.add(viewButton);
         panel.add(deleteButton);
         panel.add(logoutButton);
 
+        // -- Create dialog box --
         JOptionPane.showOptionDialog(
                 null,
                 panel,
